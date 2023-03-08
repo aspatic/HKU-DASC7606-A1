2141e5fdbde78124a1164b6f079d34f302958d0d
aspatic <yufengchen8@gmail.com>
2023-03-08 11:43:26 +0800

    finish bottleneck block

--- src/model/block.py
+++ src/model/block.py
@@ -55,13 +55,17 @@ class Bottleneck(nn.Module):
 
     def __init__(self, in_planes, planes, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
-        
+
         ##############################################################
         # TODO: Please define your layers with the BottleNeck from the paper "Deep Residual Learning for Image Recognition"
         #
         # Note: You **must not** use the nn.Conv2d here but use **redefine_conv3x3** and **redefine_conv1x1** in this script instead
         ##############################################################
-        pass
+        self.conv1 = redefine_conv1x1(in_planes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = redefine_conv3x3(planes, planes)
+        self.conv3 = redefine_conv3x3(planes, planes * Bottleneck.expansion)
+        self.bn2 = nn.BatchNorm2d(planes * Bottleneck.expansion)
 
         ###############################################################
         self.relu = nn.ReLU(inplace=True)
@@ -74,8 +78,16 @@ class Bottleneck(nn.Module):
         ##############################################################
         # TODO: Please write the forward function with your defined layers
         ##############################################################
-        out = x   # you can delete this line if it's not needed
-        pass 
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn2(out)
 
         ###############################################################
         if self.downsample is not None:
